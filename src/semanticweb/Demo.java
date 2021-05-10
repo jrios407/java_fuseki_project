980db9012b6a3074b16d2c441a8181ca31b4eb49
jrios407 <datalink407@gmail.com>
2021-05-10 20:59:52 +0530

    second commit

---
+++ src/semanticweb/Demo.java
@@ -0,0 +1,21 @@
+package semanticweb;
+
+import org.apache.jena.query.* ;
+import org.apache.jena.rdf.model.ModelFactory ;
+
+public class Demo
+{
+    static public void main(String... argv) {
+        String queryString = 
+            "SELECT * WHERE { " +
+            "    SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=2000> { " +
+            "        SELECT DISTINCT ?company where {?company a <http://dbpedia.org/ontology/Company>} LIMIT 20" +
+            "    }" +
+            "}" ;
+        Query query = QueryFactory.create(queryString) ;
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, ModelFactory.createDefaultModel())) {
+            ResultSet rs = qexec.execSelect() ;
+            ResultSetFormatter.out(System.out, rs, query) ;
+        }
+    }
+}
